511979704ffdf6a857dea22d98a00bfac52e6ebe
Renata Arruda <renata.arruda85@gmail.com>
2023-07-28 12:43:55 -0300

    feat: adicionando interface Classificavel

--- screenmatch/src/br/com/alura/screenmatch/modelos/Filme.java
+++ screenmatch/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,11 +1,17 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Filme extends Titulo {
+import br.com.alura.screenmatch.calculos.Classificavel;
+
+public class Filme extends Titulo implements Classificavel {
     private String diretor;public String getDiretor() {
         return diretor;
     }
     public void setDiretor(String diretor) {
         this.diretor = diretor;
+    }
+    @Override
+    public int getClassificacao() {
+        return (int) (pegaMedia() / 2);
     }
 }
 
