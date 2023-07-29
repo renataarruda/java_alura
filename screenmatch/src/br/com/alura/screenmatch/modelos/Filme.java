e26fb1bd3ad5a69b123358034d075b164783a0b0
Renata Arruda <renata.arruda85@gmail.com>
2023-07-29 13:12:23 -0300

    style: ajustando o código

--- screenmatch/src/br/com/alura/screenmatch/modelos/Filme.java
+++ screenmatch/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -3,7 +3,8 @@ package br.com.alura.screenmatch.modelos;
 import br.com.alura.screenmatch.calculos.Classificavel;
 
 public class Filme extends Titulo implements Classificavel {
-    private String diretor;public String getDiretor() {
+    private String diretor;
+    public String getDiretor() {
         return diretor;
     }
     public void setDiretor(String diretor) {
