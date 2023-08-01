dd0bd75f23e7da2ff15850b0a848dd9d0d307ba3
Renata Arruda <renata.arruda85@gmail.com>
2023-08-01 15:30:47 -0300

    feat: adiciona construtores e método toString

--- screenmatch/src/br/com/alura/screenmatch/modelos/Filme.java
+++ screenmatch/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -4,6 +4,9 @@ import br.com.alura.screenmatch.calculos.Classificavel;
 
 public class Filme extends Titulo implements Classificavel {
     private String diretor;
+    public Filme(String nome, int anoDeLancamento) {
+        super(nome, anoDeLancamento);
+    }
     public String getDiretor() {
         return diretor;
     }
@@ -12,7 +15,14 @@ public class Filme extends Titulo implements Classificavel {
     }
     @Override
     public int getClassificacao() {
+
         return (int) (pegaMedia() / 2);
+    }
+
+    //Sobrescrevendo o método toString para a maneira como quer o retorno
+    @Override
+    public String toString() {
+        return "Filme: " + this.getNome() + " (" + this.getAnoDeLancamento() + ")";
     }
 }
 
--- screenmatch/src/br/com/alura/screenmatch/modelos/Serie.java
+++ screenmatch/src/br/com/alura/screenmatch/modelos/Serie.java
@@ -6,6 +6,10 @@ public class Serie extends Titulo {
     private int episodiosPorTemporada;
     private int minutosPorEpisodio;
 
+    public Serie(String nome, int anoDeLancamento) {
+        super(nome, anoDeLancamento);
+    }
+
     public int getTemporadas() {
         return temporadas;
     }
@@ -40,5 +44,10 @@ public class Serie extends Titulo {
     @Override
     public int getDuracaoEmMinutos() {
         return temporadas * episodiosPorTemporada * minutosPorEpisodio;
+    }
+
+    @Override
+    public String toString() {
+       return "Série: " + this.getNome() + " (" + this.getAnoDeLancamento() + ")";
     }
 }
