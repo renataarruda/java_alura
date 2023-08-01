2bd6d7c94fe8dae9b00c0479484d2b341dcc4896
Renata Arruda <renata.arruda85@gmail.com>
2023-08-01 17:51:58 -0300

    feat: utiliza Collections e Comparable para ordenar em ordem alfabética

--- screenmatch/src/br/com/alura/screenmatch/modelos/Titulo.java
+++ screenmatch/src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -1,6 +1,6 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Titulo {
+public class Titulo implements Comparable<Titulo> {
     private String nome;
     private int anoDeLancamento;
     private boolean incluidoNoPlano;
@@ -54,5 +54,10 @@ public class Titulo {
     }
     double pegaMedia(){
         return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
+
+    @Override
+    public int compareTo(Titulo outroTitulo) {
+        return this.getNome().compareTo(outroTitulo.getNome());
     }
 }
--- screenmatch/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ screenmatch/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -5,13 +5,22 @@ import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Titulo;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
         Filme meuFilme = new Filme("Oppenheimer", 2023);
+        meuFilme.avalia(8);
         Filme meuFilme2 = new Filme("Matrix", 1999);
+        meuFilme2.avalia(10);
         var filmeDoPaulo = new Filme("Dogville",2003);
+        filmeDoPaulo.avalia(10);
         Serie serie = new Serie("Stranger Things",2018);
+
+        //Variável de referência: utilizada para chegar a um objeto
+        //Filme f1 = filmeDoPaulo; // Não copia um novo objeto (não há uso do new). Aponta para um já existente.
+        //f1.setNome("A Origem");
+        //System.out.println(filmeDoPaulo);
 
         ArrayList<Titulo> lista = new ArrayList<>();
         lista.add(filmeDoPaulo);
@@ -20,14 +29,31 @@ public class PrincipalComListas {
         lista.add(serie);
 
         //Laço for-each
-        //for (Titulo item: lista) {
-        //    System.out.println(item);
-        //}
+        for (Titulo item: lista) {
+            System.out.println(item.getNome());
+            //Maneira de permitir o uso de um método exclusivo da classe Filme sem erro de compilação
+            if(item instanceof Filme filme){
+                System.out.println("Classificação: " + filme.getClassificacao());
+            }
+        }
 
         //Método forEach
         //lista.forEach(item -> System.out.println(item));
 
         //Method Reference (forma reduzida da expressão lambda
-        lista.forEach(System.out::println);
+        //lista.forEach(System.out::println);
+
+        ArrayList<String> buscaPorArtistas = new ArrayList<>();
+        buscaPorArtistas.add("David Tennant");
+        buscaPorArtistas.add("Viola Davis");
+        buscaPorArtistas.add("Marlon Brando");
+        System.out.println(buscaPorArtistas);
+
+        Collections.sort(buscaPorArtistas);
+        System.out.println(buscaPorArtistas);
+
+        Collections.sort(lista);
+        System.out.println(lista);
+
     }
 }
