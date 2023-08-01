dd50d604ebdd5ea2d26d66d27dcf24d30184941d
Renata Arruda <renata.arruda85@gmail.com>
2023-08-01 18:14:37 -0300

    feat: utiliza Comparator para ordenar por ano de lançamento

--- screenmatch/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ screenmatch/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -6,6 +6,7 @@ import br.com.alura.screenmatch.modelos.Titulo;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -53,6 +54,8 @@ public class PrincipalComListas {
         System.out.println(buscaPorArtistas);
 
         Collections.sort(lista);
+        System.out.println(lista);
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
         System.out.println(lista);
 
     }
