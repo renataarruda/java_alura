24852676a98598f8be9680738b7a774ba71a1d78
Renata Arruda <renata.arruda85@gmail.com>
2023-09-14 17:13:33 -0300

    criação do Record com os dados dos episódios da série

--- screenserie/src/main/java/br/com/alura/screenserie/ScreenserieApplication.java
+++ screenserie/src/main/java/br/com/alura/screenserie/ScreenserieApplication.java
@@ -1,8 +1,6 @@
 package br.com.alura.screenserie;
 
-import br.com.alura.screenserie.model.DadosSerie;
-import br.com.alura.screenserie.service.ConsumoApi;
-import br.com.alura.screenserie.service.ConverteDados;
+import br.com.alura.screenserie.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,11 +14,7 @@ public class ScreenserieApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=9008c0cc");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
